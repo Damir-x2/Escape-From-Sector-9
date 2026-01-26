ac5c5eab4bc5cee32f530ca43afbb1e6f6b7f9a7
Damir VLT <damirasdfghjkl@gmail.com>
2026-01-27 00:11:28 +0700

    Добавлено управление камерой и ограничения движения игрока в пределах игрового мира

--- views/game_view.py
+++ views/game_view.py
@@ -1,33 +1,42 @@
 import arcade
+from arcade.camera import Camera2D
 from sprites.player import Player
+
+WORLD_WIDTH = 2000
+WORLD_HEIGHT = 2000
 
 
 class GameView(arcade.View):
     def __init__(self):
         super().__init__()
+
         self.player_list = arcade.SpriteList()
         self.player = None
+        self.camera = Camera2D()
 
     def on_show_view(self):
         arcade.set_background_color(arcade.color.DARK_SLATE_GRAY)
         self.setup()
+        self.center_camera_to_player()
 
     def setup(self):
         self.player_list = arcade.SpriteList()
 
         self.player = Player()
-        self.player.center_x = self.window.width // 2
-        self.player.center_y = self.window.height // 2
+        self.player.center_x = WORLD_WIDTH // 2
+        self.player.center_y = WORLD_HEIGHT // 2
 
         self.player_list.append(self.player)
 
     def on_draw(self):
         self.clear()
 
+        self.camera.activate()
+
         arcade.draw_text(
             "GAME VIEW",
             20,
-            self.window.height - 40,
+            WORLD_HEIGHT - 40,
             arcade.color.WHITE,
             20
         )
@@ -39,14 +48,34 @@ class GameView(arcade.View):
 
         if self.player.left < 0:
             self.player.left = 0
-        if self.player.right > self.window.width:
-            self.player.right = self.window.width
+        if self.player.right > WORLD_WIDTH:
+            self.player.right = WORLD_WIDTH
 
         if self.player.bottom < 0:
             self.player.bottom = 0
-        if self.player.top > self.window.height:
-            self.player.top = self.window.height
+        if self.player.top > WORLD_HEIGHT:
+            self.player.top = WORLD_HEIGHT
 
+        self.center_camera_to_player()
+
+    def center_camera_to_player(self):
+        camera_x = max(
+            0,
+            min(
+                self.player.center_x - self.window.width / 2,
+                WORLD_WIDTH - self.window.width
+            )
+        )
+
+        camera_y = max(
+            0,
+            min(
+                self.player.center_y - self.window.height / 2,
+                WORLD_HEIGHT - self.window.height
+            )
+        )
+
+        self.camera.position = (camera_x, camera_y)
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.W:
