938a2e4b7a80e6ef130cc2190bc18d42edde1f20
Damir <160710174+Damir-x2@users.noreply.github.com>
2026-02-03 18:50:48 +0700

    Update player sprite by movement direction

--- sprites/player.py
+++ sprites/player.py
@@ -1,18 +1,21 @@
 import arcade
-import math
 
 
 class Player(arcade.Sprite):
     def __init__(self):
         super().__init__()
 
-        # размеры
-        self.width = 30
-        self.height = 30
-
-        # позиция
-        self.center_x = 400
-        self.center_y = 300
+        self.textures_map = {
+            "front": arcade.load_texture("assets/player/player_front.png"),
+            "back": arcade.load_texture("assets/player/player_back.png"),
+            "right": arcade.load_texture("assets/player/player_right.png"),
+            "left": arcade.load_texture(
+                "assets/player/player_right.png",
+                flipped_horizontally=True,
+            ),
+        }
+        self.texture = self.textures_map["front"]
+        self.scale = 0.6
 
         self.speed = 2
 
@@ -25,24 +28,14 @@ class Player(arcade.Sprite):
     def update(self):
         self.center_x += self.change_x
         self.center_y += self.change_y
+        self.update_texture()
 
-    def draw(self):
-        angle = math.atan2(self.facing_y, self.facing_x)
-
-        tip = (
-            self.center_x + math.cos(angle) * 20,
-            self.center_y + math.sin(angle) * 20
-        )
-        left = (
-            self.center_x + math.cos(angle + 2.5) * 15,
-            self.center_y + math.sin(angle + 2.5) * 15
-        )
-        right = (
-            self.center_x + math.cos(angle - 2.5) * 15,
-            self.center_y + math.sin(angle - 2.5) * 15
-        )
-
-        arcade.draw_polygon_filled(
-            [tip, left, right],
-            arcade.color.BLUE
-        )
+    def update_texture(self):
+        if self.facing_y > 0:
+            self.texture = self.textures_map["back"]
+        elif self.facing_y < 0:
+            self.texture = self.textures_map["front"]
+        elif self.facing_x > 0:
+            self.texture = self.textures_map["right"]
+        elif self.facing_x < 0:
+            self.texture = self.textures_map["left"]
