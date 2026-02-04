2a15c9347bfa2999a3552693cc4c255dc1a6655b
Damir-x2 <160710174+Damir-x2@users.noreply.github.com>
2026-02-04 23:20:58 +0700

    Добавлена смена скина персонажа при ходьбе

--- sprites/player.py
+++ sprites/player.py
@@ -1,48 +1,47 @@
 import arcade
-import math
 
 
 class Player(arcade.Sprite):
     def __init__(self):
         super().__init__()
 
-        # размеры
-        self.width = 30
-        self.height = 30
-
-        # позиция
+        # Size and position
+        self.target_size = 30
         self.center_x = 400
         self.center_y = 300
 
         self.speed = 2
 
-        # направление (куда смотрит)
+        # Facing direction
         self.facing_x = 1
         self.facing_y = 0
 
         self.health = 100
+        self._load_textures()
+        self._apply_facing_texture()
+
+    def _load_textures(self):
+        self.texture_up = arcade.load_texture("assets/player/player_back.png")
+        self.texture_down = arcade.load_texture("assets/player/player_front.png")
+        self.texture_left = arcade.load_texture("assets/player/player_left.png")
+        self.texture_right = arcade.load_texture("assets/player/player_right.png")
+
+        self.scale = self.target_size / self.texture_right.width
+
+    def _apply_facing_texture(self):
+        if self.facing_y > 0:
+            self.texture = self.texture_up
+        elif self.facing_y < 0:
+            self.texture = self.texture_down
+        elif self.facing_x < 0:
+            self.texture = self.texture_left
+        else:
+            self.texture = self.texture_right
 
     def update(self):
         self.center_x += self.change_x
         self.center_y += self.change_y
+        self._apply_facing_texture()
 
     def draw(self):
-        angle = math.atan2(self.facing_y, self.facing_x)
-
-        tip = (
-            self.center_x + math.cos(angle) * 20,
-            self.center_y + math.sin(angle) * 20
-        )
-        left = (
-            self.center_x + math.cos(angle + 2.5) * 15,
-            self.center_y + math.sin(angle + 2.5) * 15
-        )
-        right = (
-            self.center_x + math.cos(angle - 2.5) * 15,
-            self.center_y + math.sin(angle - 2.5) * 15
-        )
-
-        arcade.draw_polygon_filled(
-            [tip, left, right],
-            arcade.color.BLUE
-        )
+        arcade.draw_sprite(self)
