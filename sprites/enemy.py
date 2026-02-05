e11fbc5a2349b9f2c5d5a3594149fecadecaeaa4
Damir-x2 <160710174+Damir-x2@users.noreply.github.com>
2026-02-05 23:36:26 +0700

    добавление скинов врагам

--- sprites/enemy.py
+++ sprites/enemy.py
@@ -7,9 +7,7 @@ class Enemy(arcade.Sprite):
     def __init__(self, x, y, left, right):
         super().__init__()
 
-        self.texture = arcade.make_soft_square_texture(
-            28, arcade.color.RED, 255
-        )
+        self.target_size = 45
 
         self.center_x = x
         self.center_y = y
@@ -25,7 +23,30 @@ class Enemy(arcade.Sprite):
         self.move_timer = 0.0
         self.move_interval = random.uniform(0.8, 2.5)
 
+        self.facing_x = 1
+        self.facing_y = 0
+
+        self._load_textures()
+        self._apply_facing_texture()
         self.pick_new_direction()
+
+    def _load_textures(self):
+        self.texture_up = arcade.load_texture("assets/enemies/enemie1_back.png")
+        self.texture_down = arcade.load_texture("assets/enemies/enemie1_front.png")
+        self.texture_left = arcade.load_texture("assets/enemies/enemie1_left.png")
+        self.texture_right = arcade.load_texture("assets/enemies/enemie1_right.png")
+
+        self.scale = self.target_size / self.texture_right.width
+
+    def _apply_facing_texture(self):
+        if self.facing_y > 0:
+            self.texture = self.texture_up
+        elif self.facing_y < 0:
+            self.texture = self.texture_down
+        elif self.facing_x < 0:
+            self.texture = self.texture_left
+        else:
+            self.texture = self.texture_right
 
     def pick_new_direction(self):
         directions = [
@@ -38,6 +59,9 @@ class Enemy(arcade.Sprite):
 
         self.change_x = dx * self.speed
         self.change_y = dy * self.speed
+        self.facing_x = dx
+        self.facing_y = dy
+        self._apply_facing_texture()
 
     def update(self, delta_time: float = 1 / 60):
         if self.dead:
@@ -52,6 +76,7 @@ class Enemy(arcade.Sprite):
 
         self.center_x += self.change_x
         self.center_y += self.change_y
+        self._apply_facing_texture()
 
     def die(self):
         self.dead = True
